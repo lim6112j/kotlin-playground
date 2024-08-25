1b63b309778090b3396e906fb478722b44f8aa10
byeongcheollim <lim6112j@gmail.com>
2024-08-25 14:16:41 +0900

    initial commit

---
+++ app/src/main/kotlin/org/example/random.kt
@@ -0,0 +1,13 @@
+interface RNG {
+		fun nextInt(): Pair<Int, RNG>
+}
+typealias Rand<A> = (RNG) -> Pair<A, RNG>
+
+val intR: Rand<Int> = {rng -> rng.nextInt()}
+
+fun <A> unit(a: A): Rand<A> = { rng -> a to rng }
+fun <A, B> map(s: Rand<A>, f: (A) -> B): Rand<B> =
+		{rng ->
+				val (a, rng2) = s(rng)
+				f(a) to rng2
+		}
